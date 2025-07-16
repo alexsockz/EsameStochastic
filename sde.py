8c07a31166797a8a7fbac94018ed0a7634640d0c
alexsockz <valessiobg@gmail.com>
2025-07-16 17:54:27 +0200

    fixed the problem of the distribution NOTE: must talk about this during the presentation, buch of plots

--- sde.py
+++ sde.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import time
 import multiprocessing as mp
+import warnings
+
 
 class trainModel(object):
     '''
@@ -64,21 +66,33 @@ class trainModel(object):
 
         return
       
-    def __euler_maruyama(self,tn: float, vt:float, st:float, dv_prec:float, h: float)->float:
+    def __euler_maruyama(self,tn: float, vt:float, st:float, dv_prec:float, sPrec:float, h: float)->float:
         #ito for
         #h=dt
         dW= np.random.normal()*np.sqrt(h)
-        sigmahat=np.sqrt((vt*(self.vmax-vt))/(self.vcruise*(self.vmax-self.vcruise)))*self.sigma
+        try:
+            sigmahat=np.sqrt((vt*(self.vmax-vt))/(self.vcruise*(self.vmax-self.vcruise)))*self.sigma
+
+            dsigmahat=(self.sigma*dv_prec*(self.vmax-(2*vt)))/np.sqrt(4*self.vcruise*(self.vmax-self.vcruise)*vt*(self.vmax-vt))
+        except RuntimeWarning:
+            print(self.a*(sPrec-st)," ",self.b*(self.vcruise-vt),"vt: ",vt, " ",(vt*(self.vmax-vt))/(self.vcruise*(self.vmax-self.vcruise)))
+            sigmahat=0
+            dsigmahat=0
+            #TODO: need to change ito to stratonovich
         
-        dsigmahat=(self.sigma*dv_prec*(self.vmax-(2*vt)))/np.sqrt(4*self.vcruise*(self.vmax-self.vcruise)*vt*(self.vmax-vt))
-        
-        #TODO: need to change ito to stratonovich
-        
+
+        #PROBLEMA se la distanza tra sPrec e st è troppo elevata sovrasta la tendenza alla media e quindi devo limitare a,
+        #SOLUZIONE min(vt, 40) se 40 allora a=0
+        #ALTERNATIVA PIU FICA, LIMITARE sPrec-st con una sigmoide
+        limiter=1 / (1 + np.exp(-self.vmax+vt))
+        a=(self.b*(self.vcruise-vt)+ self.a*(sPrec-st)*limiter)
+       
         # ito
-        # dv = (self.b*(self.vcruise-vt)+ self.a*(self.vcruise*tn-st))*h + sigmahat*dW
+
+        # dv = a*h + sigmahat*dW
         # ds = vt*h
         
-        a=(self.b*(self.vcruise-vt)+ self.a*(self.vcruise*tn-st))
+        #stratonovich
         
         dv=(a+0.5*sigmahat*dsigmahat)*h+sigmahat*dW
         ds=vt*h
@@ -103,59 +117,69 @@ class trainModel(object):
         if T==0:
             return np.array([self.vcruise])
         
-        tobreak = False
+        tobreak = np.full(self.Ntrains+1, False)
         #Setup step lenght and traj array
         h = T/N
-        v = np.zeros(N+1,dtype=float)
-        v[0] = self.vcruise
-        s = np.zeros(N+1,dtype=float)
-        s[0] = 0 #distance from the position if speed was constant
+        v = np.zeros((self.Ntrains+1,N+1),dtype=float)
+        v[:,0].fill(self.vcruise)
+
+        s = np.zeros((self.Ntrains+1,N+1),dtype=float)
+
+        #one for everyone
         time = np.zeros(N+1,dtype=float)
         time[0] = 0
-        cs= np.zeros(N+1,dtype=float)
-        cs[0] = 0
-        dist= np.zeros(N+1,dtype=float)
-        dist[0] = self.x0
-        dv=0
-        ds=0
-        breakedAt=[]
+
+        dist= np.zeros((self.Ntrains+1,N+1),dtype=float)
+        dist[:,0].fill(self.x0)               #TODO: in the future maybe transform x0 in an array with a specific distance for every train
+        dist[0,0]=0
+        
+        dv=np.zeros(self.Ntrains+1,dtype=float)
+        ds=np.zeros(self.Ntrains+1,dtype=float)
+
+        breakedAt=np.empty((self.Ntrains+1,), dtype=object)
+        breakedAt.fill([])
 
         #Setup random generator
         rng = Generator(PCG64())
         for i in range(1,N+1):
             time[i] = i*h
-            if tobreak:
-                dv, ds = self.__determ3insticChange(self.breaking, v[i-1], h)
-            else:    
-                dv, ds = self.__euler_maruyama(time[i-1],v[i-1],s[i-1], dv, h)
-    
-            v[i] = v[i-1] + dv
-            s[i] = s[i-1] + ds
-            cs[i]= self.vcruise*(time[i])
-            dist[i]= self.x0+cs[i]-s[i] 
+            v[0,i]= self.vcruise
+            s[0,i]= self.vcruise*(time[i])
+            for train in range(1, self.Ntrains+1):
+                if tobreak[train]:
+                    dv[train], ds[train] = self.__determinsticChange(self.breaking, v[train,i-1], h)
+                else:    
+                    dv[train], ds[train] = self.__euler_maruyama(time[i-1],v[train, i-1],s[train, i-1], dv[train], s[train-1,i-1], h)
+        
+                v[train, i] = v[train, i-1] + dv[train]
+                if v[train, i]<=0:
+                    v[train, i]=0
+                    dv[train]=-v[train, i-1]
+                s[train, i] = s[train, i-1] + ds[train]
+                dist[train, i]= self.x0+s[train-1,i]-s[train,i] 
 
-            if dist[i] < self.dmin:
-                if tobreak==False:
-                    breakedAt.append(i)
-                tobreak =True
-            elif dist[i] >= self.x0:
-                tobreak = False
-        return v, s, cs, dist, time, breakedAt
+                if dist[train, i] < self.dmin and train!=1:
+                    if tobreak[train] == False:
+                        breakedAt[train].append(i)
+                    tobreak[train] = True
+                elif dist[train, i] >= self.x0:
+                    tobreak[train] = False
+        return v, s, dist, time, breakedAt
     
     def create_plots(self, plot, vtraj, distraj, ttraj):
         fig, (ax_vel, ax_dist) = plot
-        ax_vel.set_ylim(15, 40) #da cambiare se si vuole
-        ax_dist.set_ylim(3000, 4500)  #da cambiare se si vuole
+        #ax_vel.set_ylim(10, 40) #da cambiare se si vuole
+        #ax_dist.set_ylim(3000, 4500)  #da cambiare se si vuole
         ax_vel.plot(ttraj, vtraj, color='gray', linewidth=0.5)
         ax_dist.plot(ttraj, distraj, color='gray', linewidth=0.5)
 
         return fig, (ax_vel, ax_dist)
 
 #---------------------------------------------------------------------------------------------------------------    
-for s in ["DMR","CIR"]:
+for s in ["CIR"]:
     all_headway = []
     all_speed = []
-
+    
     # Create the first figure with 2 subplots for time series plots
     fig1 = plt.subplots(1, 2, figsize=(12, 5))
     
@@ -169,7 +193,9 @@ for s in ["DMR","CIR"]:
     
     Nsim = 500
     #Ntreni, b, vcruise, a, vmax, x0, min dist, sigma, breaking, select_model
-    args= (1,0.02,35,0.0005,40,3200,3000, 0.1, 0.55, s)
+
+    trainToFollow=4
+    args= (4,0.02,35,0.0005,40,3200,3000, 0.1, 0.55, s)
     period=(5000, 5000)
     
     system = trainModel(*args)
@@ -180,15 +206,19 @@ for s in ["DMR","CIR"]:
             #T    #N
     distr_of_breaks=np.zeros(period[1]+1)
 
+    warnings.filterwarnings("error")
     for i in range(Nsim):
-        vtraj, straj, cstraj, distraj, ttraj, breakedAt = system.simulateTraj(*period)
-        for point in breakedAt:
-            distr_of_breaks[point]+=1
-            break
-        if(i%25==0):
-            system.create_plots(fig1, vtraj, distraj, ttraj)
-        all_speed.append(vtraj)
-        all_headway.append(distraj)
+        vtraj, straj, distraj, ttraj, breakedAt = system.simulateTraj(*period)
+        if(breakedAt[trainToFollow]!=[]):
+            distr_of_breaks[breakedAt[trainToFollow][0]]+=1
+        # for point in breakedAt[trainToFollow]:
+        #     #print(point)
+        #     distr_of_breaks[point]+=1
+        if(i%14==0):
+            system.create_plots(fig1, vtraj[trainToFollow], distraj[trainToFollow], ttraj)
+        all_speed.append(vtraj[trainToFollow])
+        all_headway.append(distraj[trainToFollow])
+    warnings.filterwarnings("ignore")
 
     headway_q = np.concatenate(all_headway) / 1000
     speed_q = np.concatenate(all_speed)
@@ -200,8 +230,8 @@ for s in ["DMR","CIR"]:
     h = ax2.hist2d(headway_q, speed_q, bins=50, norm=mpl.colors.LogNorm(), cmap=mpl.cm.Blues)
     ax2.set_xlabel("headway (km)")
     ax2.set_ylabel("speed follower (m/s)")
-    ax2.set_xlim(3, 4.5)
-    ax2.set_ylim(18,42)
+    #ax2.set_xlim(3, 4.5)
+    #ax2.set_ylim(10,42)
     cb = fig2.colorbar(h[3],ax=ax2)
     cb.set_label("Bin Counts")
 
@@ -220,6 +250,7 @@ for s in ["DMR","CIR"]:
     plt.tight_layout()
 
     filedata= '_'.join([str(s) for s in args])
-    fig1[0].savefig("plots/"+filedata+"_5000_stratonovich_speed_and_distance.svg",format='svg')
-    fig2.savefig("plots/"+filedata+"_5000_stratonovich_speed_to_distance.svg",format='svg')
-    fig3.savefig("plots/"+filedata+"_5000_stratonovich_pdf_first_break.svg",format='svg')
+    fig1[0].savefig("plots/"+filedata+"_5000_with_limiter_both_sides_stratonovich_speed_and_distance.svg",format='svg')
+    fig2.savefig("plots/"+filedata+"_5000_with_limiter_both_sides_tratonovich_speed_to_distance.svg",format='svg')
+    fig3.savefig("plots/"+filedata+"_5000_with_limiter_both_sides_tratonovich_pdf_first_break.svg",format='svg')
+    plt.show()
